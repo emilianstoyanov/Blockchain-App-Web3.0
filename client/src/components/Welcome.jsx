a5064f35422ded90a511306991b1083780d49229
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-03-19 11:47:46 +0200

    update ethereum card

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -46,6 +46,20 @@ const Welcome = () => {
                             </div>
                         </div>
                 </div>
+                <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+                    <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorpism">
+                        <div className="flex justify-between flex-col w-full h-full">
+                            <div className="flex justify-between items-start">
+                                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                                    <SiEthereum fontSize={21} color="#fff" />
+                                </div>
+                                <BsInfoCircle fontSize={17} color="#fff" />
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
