918ec0b613ffbe5b9e114915d88f689a7492a9b1
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-03-19 11:03:34 +0200

    update connect wallet button

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -24,7 +24,7 @@ const Welcome = () => {
                         onClick={connetWallet}
                         className="flex flex-row justify-center items-center my-5 bg-[#2595e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
                         >
-                            Connect Wallet
+                            <p className="text-white text-base font-semibold">Connect Wallet</p>
                         </button>
                     
                 </div>
