1f9d994451dee722e305ad5de999a23dcfbbeb00
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-03-19 11:35:13 +0200

    update welcome page

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -4,6 +4,8 @@ import { BsInfoCircle } from 'react-icons/bs';
 
 import { Loader } from './';
 
+const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+
 const Welcome = () => {
 
     const connetWallet = () => {
@@ -26,7 +28,23 @@ const Welcome = () => {
                         >
                             <p className="text-white text-base font-semibold">Connect Wallet</p>
                         </button>
-                    
+
+                        <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
+                            <div className={`rounded-tl-2xl ${commonStyles}`}>
+                                Reliability
+                            </div>
+                            <div className={commonStyles}>Security</div>
+                            <div className={`rounded-tr-2xl ${commonStyles}`}>
+                                Ethereum
+                            </div>
+                            <div className={`rounded-bl-2xl ${commonStyles}`}>
+                                Web 3.0
+                            </div>
+                            <div className={commonStyles}>Low fees</div>
+                            <div className={`rounded-br-2xl ${commonStyles}`}>
+                                Blockchain
+                            </div>
+                        </div>
                 </div>
             </div>
         </div>
