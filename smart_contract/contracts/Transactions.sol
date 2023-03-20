0128b76814c4f2445e17e0fdd8fbab084675b3b6
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-03-20 17:05:44 +0200

    create solidity smart contract

---
+++ smart_contract/contracts/Transactions.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+
+contract Transtactions {
+    uint256 transactionCount;
+
+    event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
+
+    struct TransterStruct {
+        address sender;
+        address receiver;
+        uint amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
+
+    TransterStruct[] transactions;
+
+    function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public {
+        transactionCount += 1;
+        transactions.push(TransterStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
+    }
+
+    function getAllTransactions() public view returns (TransterStruct[] memory) {
+        // return transactions;
+
+    }
+
+    function getTransactionCount() public view returns (uint256) {
+        // return transactionCounter;
+
+
+    }
+}
