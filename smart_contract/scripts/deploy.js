7e89a204ee591e6e823f1a88b2aeac1a69103bcf
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-03-21 11:05:18 +0200

    update deploy.js

--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -10,7 +10,7 @@ const main = async () => {
 const runMain = async () => {
   try {
     await main();
-    process.exit();
+    process.exit(0);
   } catch (error) {
     console.error(error);
     process.exit(1);
