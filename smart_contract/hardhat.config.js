cda8f3622dfe47a821188ca724b92c7c493a1249
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-03-21 11:14:43 +0200

    update hardhat config

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+// 
 
-/** @type import('hardhat/config').HardhatUserConfig */
+
+require('@nomiclabs/hardhat-waffle')
+
 module.exports = {
-  solidity: "0.8.18",
-};
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: ''
+    }
+  }
+}
