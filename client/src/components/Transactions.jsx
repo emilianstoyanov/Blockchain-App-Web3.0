0ef09ddceef8fff4fd746a0c0d78c9a3e7f2cfbc
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-03-25 23:23:45 +0200

    update transactions.jsx

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,8 +1,23 @@
 const Transactions = () => {
     return (
-        <h1>Transactions</h1>
+      <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
+        <div className="flex flex-col md:p-12 py-12 px-4">
+         
+            <h3 className="text-white text-3xl text-center my-2">
+              Latest Transactions
+            </h3>
+         
+            <h3 className="text-white text-3xl text-center my-2">
+              Connect your account to see the latest transactions
+            </h3>
+         
+  
+          <div className="flex flex-wrap justify-center items-center mt-10">
+           
+          </div>
+        </div>
+      </div>
     );
-}
-
-
-export default Transactions;
+  };
+  
+  export default Transactions;
