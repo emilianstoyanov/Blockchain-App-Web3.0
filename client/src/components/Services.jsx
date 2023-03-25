35a80283da8f7d2b769c2f11334a44e52076b833
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-03-25 22:42:23 +0200

    update service card

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -2,6 +2,23 @@ import { BsShieldFillCheck } from "react-icons/bs";
 import { BiSearchAlt } from "react-icons/bi";
 import { RiHeart2Fill } from "react-icons/ri";
 
+
+const ServiceCard = ({ color, title, icon, subtitle }) => (
+    <div className="flex flex-row justify-start items-start white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl">
+      <div className={`w-10 h-10 rounded-full flex justify-center items-center ${color}`}>
+        {icon}
+      </div>
+      <div className="ml-5 flex flex-col flex-1">
+        <h3 className="mt-2 text-white text-lg">{title}</h3>
+        <p className="mt-1 text-white text-sm md:w-9/12">
+          {subtitle}
+        </p>
+      </div>
+    </div>
+  );
+
+
+
 const Services = () => {
     return (
         <div className="flex w-full justify-center items-center gradient-bg-services">
@@ -15,7 +32,12 @@ const Services = () => {
                 </div>
             </div>
             <div className="flex-1 flex flex-col justify-start items-center">
-
+                < ServiceCard 
+                color="bg-[#2952E3]"
+                title="Security gurantee"
+                icon={<BsShieldFillCheck fontSize={21} className="text-white" />}
+                subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our products" 
+                />
             </div>
         </div>
     );
