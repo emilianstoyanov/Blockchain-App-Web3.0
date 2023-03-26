c1c3538807d544d002d2ba6c550f8b7fc41b3642
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-03-26 12:56:59 +0300

    update transactions.jsx

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -8,7 +8,20 @@ import dummyData from "../utils/dummyData";
 
 
 const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
-    return 'transaction';
+    return (
+        <div className="bg-[#181918] m-4 flex flex-1
+          2xl:min-w-[450px]
+          2xl:max-w-[500px]
+          sm:min-w-[270px]
+          sm:max-w-[300px]
+          min-w-full
+          flex-col p-3 rounded-md hover:shadow-2xl"
+        >
+          <div className="flex flex-col items-center w-full mt-3">
+           
+          </div>
+        </div>
+    );
 };
 
 
