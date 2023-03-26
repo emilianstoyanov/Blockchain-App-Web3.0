1ee62e3d910800659405d5032968d5303a390b1e
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-03-26 12:45:18 +0300

    update transactions.jsx

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,19 +1,40 @@
+import React, { useContext } from "react";
+
+import { TransactionContext } from "../context/TransactionContext";
+
+
+import dummyData from "../utils/dummyData";
+// import { shortenAddress } from "../utils/shortenAddress";
+
+
+const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
+    return 'transaction';
+};
+
+
+
+
+
 const Transactions = () => {
+    const { currentAccount } = useContext(TransactionContext);
+  
     return (
       <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
         <div className="flex flex-col md:p-12 py-12 px-4">
-         
+          {currentAccount ? (
             <h3 className="text-white text-3xl text-center my-2">
               Latest Transactions
             </h3>
-         
+          ) : (
             <h3 className="text-white text-3xl text-center my-2">
               Connect your account to see the latest transactions
             </h3>
-         
+          )}
   
           <div className="flex flex-wrap justify-center items-center mt-10">
-           
+            {dummyData.reverse().map((transaction, i) => (
+              <TransactionsCard key={i} {...transaction} />
+            ))}
           </div>
         </div>
       </div>
@@ -21,3 +42,4 @@ const Transactions = () => {
   };
   
   export default Transactions;
+  
