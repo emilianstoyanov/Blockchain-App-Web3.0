5b2eb2f56fb49a3f1e93b3dbfc12d031aaca833b
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-03-26 14:37:31 +0300

    update loader and navbar

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -2,13 +2,12 @@
 
 pragma solidity ^0.8.0;
 
-
 contract Transactions {
     uint256 transactionCount;
 
     event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
-
-    struct TransterStruct {
+  
+    struct TransferStruct {
         address sender;
         address receiver;
         uint amount;
@@ -17,16 +16,16 @@ contract Transactions {
         string keyword;
     }
 
-    TransterStruct[] transactions;
+    TransferStruct[] transactions;
 
     function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public {
         transactionCount += 1;
-        transactions.push(TransterStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
+        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
 
         emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
     }
 
-    function getAllTransactions() public view returns (TransterStruct[] memory) {
+    function getAllTransactions() public view returns (TransferStruct[] memory) {
         return transactions;
     }
 
